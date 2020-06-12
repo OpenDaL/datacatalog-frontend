bd269a3d4fb376754e6388bf5e41c4ee508b7ac4
Tom Brouwer <tombrouwer@outlook.com>
2020-06-12 19:12:41 +0200

    Update to 20200612

--- django/datacatalog/dcsearch/sitemaps.py
+++ django/datacatalog/dcsearch/sitemaps.py
@@ -14,7 +14,7 @@ class StaticViewSitemap(sitemaps.Sitemap):
             {
                 'name': 'search',
                 'priority': 1.0,
-                'lastmod': datetime.datetime(2020, 6, 5),
+                'lastmod': datetime.datetime(2020, 6, 12),
                 'changefreq': 'weekly'
             },
             {

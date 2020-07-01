c022cf0585c54ddc349a8f689097de258ab2d08c
Tom Brouwer <tombrouwer@outlook.com>
2020-07-01 15:16:19 +0200

    Fix header image not loading on slower connections

--- django/datacatalog/dcsearch/static/dcsearch/js/common.js
+++ django/datacatalog/dcsearch/static/dcsearch/js/common.js
@@ -6,16 +6,20 @@ const headerElement = document.querySelector('#onboarding, #page-header');
 
 // Set an in memory image to track if webp image can be loaded
 const memImg = document.createElement('img');
-const headerStyle = window.getComputedStyle(headerElement, false);
-const curImageUrl = headerStyle.backgroundImage.slice(5,-2);
-memImg.src = curImageUrl;
+
+window.onload = (event) => {
+  const headerStyle = window.getComputedStyle(headerElement, false);
+  var curImageUrl = headerStyle.backgroundImage.slice(5,-2);
+  memImg.src = curImageUrl;
+}
 
 memImg.addEventListener('error', (ev) => {
+    // curImageUrl can be used, since error can only occur after 'onload' event
     fallBackImage = curImageUrl.replace('.webp', '.jpg');
     headerElement.style.backgroundImage = `url("${fallBackImage}")`;
 })
 
-// Open hanburger menu on click
+// Open hamburger menu on click
 hmbrgr.addEventListener('click', function(e) {
     menu.classList.toggle('open-menu');
     header.classList.toggle('open-menu');
